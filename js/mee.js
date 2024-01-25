1d5ab2906180799799bbdcdd73b0c79c10f2df53
lauLanez <laulanyumbayire@gmail.com>
2024-01-24 19:55:48 -0600

    not yet responsive

---
+++ js/mee.js
@@ -0,0 +1,20 @@
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+    list.forEach(function(item) {
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+}
+
+list.forEach(item => item.addEventListener("mouseover", activeLink));
+
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+};
+  
